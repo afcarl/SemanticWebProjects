1878e217b65954285a9d56175df94d8661eda717
priyakhokher <pnk230@nyu.edu>
2016-09-03 16:30:57 -0700

    combined content

--- BF/LGBT_Network/combine_two_pickledData.py
+++ BF/LGBT_Network/combine_two_pickledData.py
@@ -4,12 +4,21 @@ import pickle
 PIK1 = "pickle1.dat"
 PIK2 = "pickle2.dat"
 
+PIK = "contentScraped.dat"
+
 with open(PIK1, "r") as f:
-	data = pickle.load(f)
+	data1 = pickle.load(f)
 
 with open(PIK2, "r") as f1:
-	data1 = pickle.load(f1)
+	data2 = pickle.load(f1)
 
-print data[0]
+print data2[0]
 print "\n"
 print data1[0]
+
+data = data1 + data2
+
+with open(PIK, "w") as f:
+	pickle.dump(data,f)
+
+
